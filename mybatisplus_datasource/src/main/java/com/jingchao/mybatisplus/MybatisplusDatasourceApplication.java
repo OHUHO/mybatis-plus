bbf5ae8304fff7a84948c8df4e6d5863f9ebf59b
JingChao <jc.jingchao@qq.com>
2022-10-13 13:54:18 +0800

    create mybatisplus_datasource

---
+++ mybatisplus_datasource/src/main/java/com/jingchao/mybatisplus/MybatisplusDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.jingchao.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisplusDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisplusDatasourceApplication.class, args);
+    }
+
+}
---
+++ mybatisplus_datasource/src/test/java/com/jingchao/mybatisplus/MybatisplusDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jingchao.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisplusDatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
